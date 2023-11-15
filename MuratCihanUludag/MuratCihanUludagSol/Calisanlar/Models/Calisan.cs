bfb3f24d217b8d1cfe25710cd17864d1ac6a6bae
MuratCihanUludag <muratcihanuludag@hotmail.com>
2023-11-15 12:13:30 +0300

    Create Solucation and FormApplication,  add class name = Calisanlar.cs

---
+++ MuratCihanUludag/MuratCihanUludagSol/Calisanlar/Models/Calisan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calisanlar.Models
+{
+    internal class Calisan
+    {
+        public long Tc { get; set; }
+        public string Ad {  get; set; } 
+        public string Soyad { get; set; }
+        public int Yas { get; set; }    
+    }
+}
