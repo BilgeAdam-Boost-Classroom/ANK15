3d274e7a2ce47dcfed9e43a7cecb2d2cd8ea3681
EnesOzturk4 <ozturkenes614@gmail.com>
2023-11-16 00:11:58 +0300

    d

---
+++ EnesOzturk/EnesOzturk/Odev_3/Calisan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_3
+{
+    public class Calisan
+    {
+
+        public string Ad {  get; set; }
+        public string Soyad { get; set; }
+        public int Yas { get; set; }
+        public long Tc { get; set; }
+
+    }
+}
