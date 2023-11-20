bde577f31c6409a9e2e0b717cfe7ef29a6b83fd7
BurakAKYIL <mburakakyil@gmail.com>
2023-11-20 18:44:21 +0300

    Odev4

---
+++ Burak.Akyil/Odev4/Kopek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev4
+{
+    public class Kopek : Canli
+    {
+        public Kopek(string ad, int yas) : base(ad, yas) { }
+        public override void SesCikar()
+        {
+            Console.WriteLine("Hav hav!");
+        }
+    }
+}
