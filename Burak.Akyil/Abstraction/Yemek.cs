f20bf6ef068c3a6d76a04ad3da4a7d7d70bd9508
BurakAKYIL <mburakakyil@gmail.com>
2023-11-21 12:32:36 +0300

    Abstraction

---
+++ Burak.Akyil/Abstraction/Yemek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Yemek
+    {
+        public Yemek()
+        {
+            Console.WriteLine("Yemek yapıcısı");
+        }
+        public bool DiyetMi { get; set; }
+        public double SosMiktari { get; set; }
+
+        public abstract void YemekHazirlan();
+
+        public virtual void Sunul()
+        {
+            Console.WriteLine("Servis ediliyor...");
+        }
+    }
+}
