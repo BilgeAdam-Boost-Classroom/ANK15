c5ca8d8f5a7a9192e81d032b29cc062dab93e201
OzturkEnes4 <ozturkenes614@gmail.com>
2023-11-21 12:23:03 +0300

    abstract

---
+++ EnesOzturk/EnesOzturk/Abstracts_uygulama/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Abstracts_uygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
---
+++ EnesOzturk/EnesOzturk/Abstracts_uygulama/İnsan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracts_uygulama
+{
+    public class İnsan : Tur
+    {
+        public override void FamilyaGoster()
+        {
+            Console.WriteLine("familya ");
+        }
+        public override void CanliTuruGoster()
+        {
+            Console.WriteLine("canlı türü");
+        }
+    }
+}
