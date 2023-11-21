f54ce50e0c8b9bc70a7cc04cc652db5d72ecec81
SibelEv <sibelb76@gmail.com>
2023-11-21 06:06:57 +0300

    kisi bilgisi

---
+++ SibelDemir/Kisi_bilgisi/Kisi_bilgisi/Kisi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kisi_bilgisi
+{
+    internal class Kisi
+    {
+        public Kisi(string ad,string soyad)
+        {
+            Ad = ad;
+            Soyad = soyad;
+        }
+
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+}
