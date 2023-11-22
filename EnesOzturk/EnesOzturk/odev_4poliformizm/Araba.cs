449f43c5707dd0be192c8df9a0e1ba76732ca52e
EnesOzturk4 <ozturkenes614@gmail.com>
2023-11-22 22:10:06 +0300

    poliformizmodev4

---
+++ EnesOzturk/EnesOzturk/odev_4poliformizm/Araba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace odev_4poliformizm
+{
+    public class Araba:MotorluTasit
+    {
+
+        public override void Git()
+        {
+            Console.WriteLine("Araba Gidiyor");
+        }
+
+    }
+}
