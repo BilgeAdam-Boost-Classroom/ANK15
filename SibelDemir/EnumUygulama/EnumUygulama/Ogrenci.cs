63b2650fe9822ae12a023682cdbb03cba387f543
sibelbabayigit <sibelb76@gmail.com>
2023-11-23 12:39:12 +0300

    enum uygulamalar

---
+++ SibelDemir/EnumUygulama/EnumUygulama/Ogrenci.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumUygulama
+{
+   public class Ogrenci
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string No { get; set; }
+        public Durum DersDurumu { get; set; }
+
+    }
+}
