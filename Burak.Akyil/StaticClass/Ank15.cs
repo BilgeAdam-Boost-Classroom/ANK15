ed25a2bc6d67e246c77371f0d27c782f0ca9094c
BurakAKYIL <mburakakyil@gmail.com>
2023-11-24 12:31:13 +0300

    Static

---
+++ Burak.Akyil/StaticClass/Ank15.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    public static class Ank15
+    {
+        public static string Nufus { get; set; }
+        public static double Ucgen(params int[] sayilar)
+        {
+            double toplam = 0;
+            for (int i = 0; i < sayilar.Length; i++)
+                toplam += sayilar[i];
+            return (toplam/sayilar.Length) * 3; 
+        }
+        public static double Kare(double x, double y)
+        {
+            return (2 * x) + (y + 2);
+        }
+        public static string Daire(string kelime)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            if (kelime.EndsWith("y"))
+            {
+                sb.Append(kelime.Substring(0, kelime.Length - 1)).Append("ies");
+            }
+            else if (!kelime.EndsWith("s"))
+            {
+                sb.Append(kelime).Append("s");
+            }
+            else
+            {
+                sb.Append(kelime).Append("es");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Burak.Akyil/StaticClass/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StaticClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ank15.Nufus = "13";
+            Console.WriteLine(Ank15.Nufus);
+            Console.WriteLine(Ank15.Ucgen(5));
+            Console.WriteLine(Ank15.Kare(5, 8));
+            Console.WriteLine(Ank15.Daire("Care"));
+            Console.WriteLine(Ank15.Daire("Identity"));
+        }
+    }
+}
