a30144aeb83f8294d42c8618b81ec2bd12cbdc42
BurakAKYIL <mburakakyil@gmail.com>
2023-11-25 20:39:26 +0300

    Odev7

---
+++ Burak.Akyil/Odev7_3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Odev7_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            List<IPiece> pieces = new List<IPiece>();
+            
+        }
+    }
+}
