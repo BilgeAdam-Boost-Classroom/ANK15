a3451493abaf645691376a235a4dc267eda2fd94
BurakAKYIL <mburakakyil@gmail.com>
2023-11-30 12:41:49 +0300

    Burger

---
+++ Burak.Akyil/BurgerMenu/BurgerMenu/AnaForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BurgerMenu
+{
+    public partial class AnaForm : Form
+    {
+        public AnaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGoster(Form gosterilecekForm)
+        {
+            gosterilecekForm.StartPosition = 0;
+            if (!MdiChildren.Contains(gosterilecekForm))
+            {
+                gosterilecekForm.MdiParent = this;
+            }
+            foreach (Form altForm in MdiChildren)
+            {
+                if(gosterilecekForm.Text == altForm.Text)
+                {
+                    altForm.Show();
+                }
+                else
+                {
+                    altForm.Close();
+                }
+            }
+        }
+
+        private void SiparisEkle_Click(object sender, EventArgs e)
+        {
+            FormGoster(new SiparisEkleme());
+        }
+
+        private void SiparisBilgileri_Click(object sender, EventArgs e)
+        {
+            FormGoster(new SiparisBilgileri());
+        }
+
+        private void MenuEkle_Click(object sender, EventArgs e)
+        {
+            FormGoster(new MenuEkleme());
+        }
+
+        private void EkstraMalzemeEkle_Click(object sender, EventArgs e)
+        {
+            FormGoster(new EkstraMalzemeEkleme());
+        }
+    }
+}
---
+++ Burak.Akyil/BurgerMenu/BurgerMenu/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerMenu
+{
+    public class Menu
+    {
+        public string Ad { get; set; }
+        public decimal Fiyat { get; set; }
+        public Boyut Boyut { get; set; }
+    }
+}
