9e467347665cc9b0675ff96f285cb93dfcdf49a0
BurakAKYIL <mburakakyil@gmail.com>
2024-01-02 13:58:43 +0300

    LINQ gorevler

---
+++ Burak.Akyil/NorthwindDBFrist/Models/Gorev2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthwindDBFrist.Models
+{
+    public partial class Gorev2
+    {
+        public string ProductName { get; set; } = null!;
+        public string? CategoryName { get; set; }
+    }
+}
---
+++ Burak.Akyil/NorthwindDBFrist/Models/Gorev3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthwindDBFrist.Models
+{
+    public partial class Gorev3
+    {
+        public string? ProductName { get; set; }
+        public string CategoryName { get; set; } = null!;
+    }
+}
