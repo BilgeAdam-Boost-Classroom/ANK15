7cbea5f6a3261fa9d56faabc2631b75e4efe890f
BurakAKYIL <mburakakyil@gmail.com>
2024-01-02 19:21:10 +0300

    Odev12 Sorulacak

---
+++ Burak.Akyil/Odev12/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Odev12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            NORTHWNDContext _db = new NORTHWNDContext();
+        }
+    }
+}
